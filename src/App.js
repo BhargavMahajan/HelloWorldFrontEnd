730593de33289f124d92c4ab5b07ad50cfaf04d8
Abhishek Chaudhary <abhishekkhutail@gmail.com>
2020-09-26 02:24:16 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react';
 
 function App() {
+
+  const [error, setError] = useState(null);
+  const [data, setData] = useState({});
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setData(result);
+        }
+      )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.greeting}
     </div>
   );
+  
 }
 
 export default App;
